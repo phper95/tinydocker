cdbe05f8948c57a4fa1db80f11ad09d82fe6322f
phper95 <2550702985@qq.com>
2025-01-11 23:34:58 +0800

    3-13

--- gobase/3-13/main.go
+++ gobase/3-13/main.go
@@ -1,10 +1,29 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"sync"
+)
 
 func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 }
-func main() {
 
+func main() {
+	m := make(map[int]int)
+	var wg sync.WaitGroup
+
+	// 启动多个 goroutine 并发写 Map
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func(key int) {
+			defer wg.Done()
+			m[key] = key // 并发写入
+		}(i)
+	}
+
+	wg.Wait()
+
+	fmt.Println("done")
 }

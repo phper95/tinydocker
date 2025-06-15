93244a3caeb747ef2906171e486e1ea76a899afd
phper95 <you@example.com>
2025-06-15 10:52:23 +0800

    6-1

--- namespace/pid.go
+++ namespace/pid.go
@@ -1,22 +1,43 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 func main() {
-	// 使用 unshare 命令创建一个新的 PID namespace 并运行 sh
-	cmd := exec.Command("unshare", "--pid", "sh", "-c", "echo 'In new PID namespace'; sleep 10 & exec sh")
+	fmt.Println("PID + Mount Namespace Demo")
 
-	// 设置标准输入输出为当前进程的标准输入输出
+	// 显示当前进程ID
+	fmt.Printf("Parent PID: %d\n", os.Getpid())
+
+	// 设置子进程的命名空间标志
+	cmd := exec.Command("/bin/bash")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWPID | syscall.CLONE_NEWNS, // 创建新的PID和mount namespace
+	}
+
+	// 设置子进程的输入/输出
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	// 运行命令
-	if err := cmd.Run(); err != nil {
-		log.Fatalf("Failed to run command: %v", err)
+	// 启动子进程
+	if err := cmd.Start(); err != nil {
+		fmt.Printf("Error starting command: %v\n", err)
+		return
+	}
+
+	fmt.Println("Child process started with new PID and mount namespace")
+	fmt.Println("In the child shell:")
+	fmt.Println("1. Run 'echo $$' to see PID 1")
+	fmt.Println("2. Run 'mount -t proc proc /proc' to setup proper /proc")
+	fmt.Println("3. Then 'ps aux' will only show namespace processes")
+
+	// 等待子进程结束
+	if err := cmd.Wait(); err != nil {
+		fmt.Printf("Command finished with error: %v\n", err)
 	}
 }
--- namespace/uts.go
+++ namespace/uts.go
@@ -1,22 +1,50 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 func main() {
-	// 使用 unshare 命令创建一个新的 UTS namespace 并设置 hostname
-	cmd := exec.Command("unshare", "--uts", "sh", "-c", "hostname newhostname; exec sh")
+	fmt.Println("UTS Namespace Demo")
 
-	// 设置标准输入输出为当前进程的标准输入输出
+	// 显示当前主机名
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Printf("Error getting hostname: %v\n", err)
+		return
+	}
+	fmt.Printf("Current hostname: %s\n", hostname)
+
+	// 设置子进程的命名空间标志
+	cmd := exec.Command("/bin/bash")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS, // 创建新的UTS namespace
+	}
+
+	// 设置子进程的输入/输出
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	// 运行命令
-	if err := cmd.Run(); err != nil {
-		log.Fatalf("Failed to run command: %v", err)
+	// 启动子进程
+	if err := cmd.Start(); err != nil {
+		fmt.Printf("Error starting command: %v\n", err)
+		return
+	}
+
+	// 在子进程中修改主机名
+	// 注意：这需要在子进程的shell中手动执行，例如：
+	// hostname new-hostname
+
+	fmt.Println("Child process started with new UTS namespace")
+	fmt.Println("Try running 'hostname new-hostname' in the child shell")
+	fmt.Println("Then exit and check the hostname in the parent namespace")
+
+	// 等待子进程结束
+	if err := cmd.Wait(); err != nil {
+		fmt.Printf("Command finished with error: %v\n", err)
 	}
 }
